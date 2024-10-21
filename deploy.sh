8e6514c965d0d484d6c15f36c6b6f0070e890696
emrealgan <alganemre.8@gmail.com>
2024-10-21 22:49:09 +0300

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@
 cd /home/ubuntu/pera_marin || exit
 git pull origin main
 
-# Remove existing virtual environment if it exists
-if [ -d "venv" ]; then
-    rm -rf venv
-fi
-
 # Create a new virtual environment
 python3 -m venv venv
 source venv/bin/activate
